684710f23ac542401387b48a4e86577fa875bca0
Gary Zhang <garyzhang2002@hotmail.com>
2023-06-01 22:10:42 +0800

    fix: three csts and three gks

--- src/game_display.c
+++ src/game_display.c
@@ -4,13 +4,12 @@
 #include "assets.h"
 
 
-#define CASE(NAME) case NAME: id = IMG_##NAME; break;
+#define CASE(NAME) case NAME: drawBlock(i, j, IMG_##NAME); break;
 
 void drawBoard(void) {
     clear();
-    for (int i = 0; i < BOARD_HEIGHT; i++) 
+    for (int i = 0; i < BOARD_HEIGHT; i++)
         for (int j = 0; j < BOARD_WIDTH; j++) {
-            int id;
             switch (gameState.board[i][j]) {
                 CASE(CST)
                 CASE(GK)
@@ -19,6 +18,5 @@ void drawBoard(void) {
                 CASE(CST_ON_CAGE)
                 CASE(TREE)
             }
-            drawBlock(i, j, id);
-        } 
+        }
 }
